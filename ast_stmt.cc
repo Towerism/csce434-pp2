2965993081c8d716572b261c08180c233a9f7c3b
Martin Fracker <martin.frackerjr@gmail.com>
2016-09-17 22:51:40 -0500

    Add nodes for switch and case

--- ast_stmt.cc
+++ ast_stmt.cc
@@ -9,79 +9,94 @@
 
 
 Program::Program(List<Decl*> *d) {
-    Assert(d != NULL);
-    (decls=d)->SetParentAll(this);
+  Assert(d != NULL);
+  (decls=d)->SetParentAll(this);
 }
 
 void Program::PrintChildren(int indentLevel) {
-    decls->PrintAll(indentLevel+1);
-    printf("\n");
+  decls->PrintAll(indentLevel+1);
+  printf("\n");
 }
 
 StmtBlock::StmtBlock(List<VarDecl*> *d, List<Stmt*> *s) {
-    Assert(d != NULL && s != NULL);
-    (decls=d)->SetParentAll(this);
-    (stmts=s)->SetParentAll(this);
+  Assert(d != NULL && s != NULL);
+  (decls=d)->SetParentAll(this);
+  (stmts=s)->SetParentAll(this);
 }
 
 void StmtBlock::PrintChildren(int indentLevel) {
-    decls->PrintAll(indentLevel+1);
-    stmts->PrintAll(indentLevel+1);
+  decls->PrintAll(indentLevel+1);
+  stmts->PrintAll(indentLevel+1);
 }
 
-ConditionalStmt::ConditionalStmt(Expr *t, Stmt *b) { 
-    Assert(t != NULL && b != NULL);
-    (test=t)->SetParent(this); 
-    (body=b)->SetParent(this);
+ConditionalStmt::ConditionalStmt(Expr *t, Stmt *b) {    Assert(t != NULL && b != NULL);
+  (test=t)->SetParent(this);    (body=b)->SetParent(this);
 }
 
-ForStmt::ForStmt(Expr *i, Expr *t, Expr *s, Stmt *b): LoopStmt(t, b) { 
-    Assert(i != NULL && t != NULL && s != NULL && b != NULL);
-    (init=i)->SetParent(this);
-    (step=s)->SetParent(this);
+ForStmt::ForStmt(Expr *i, Expr *t, Expr *s, Stmt *b): LoopStmt(t, b) {    Assert(i != NULL && t != NULL && s != NULL && b != NULL);
+  (init=i)->SetParent(this);
+  (step=s)->SetParent(this);
 }
 
 void ForStmt::PrintChildren(int indentLevel) {
-    init->Print(indentLevel+1, "(init) ");
-    test->Print(indentLevel+1, "(test) ");
-    step->Print(indentLevel+1, "(step) ");
-    body->Print(indentLevel+1, "(body) ");
+  init->Print(indentLevel+1, "(init) ");
+  test->Print(indentLevel+1, "(test) ");
+  step->Print(indentLevel+1, "(step) ");
+  body->Print(indentLevel+1, "(body) ");
 }
 
 void WhileStmt::PrintChildren(int indentLevel) {
-    test->Print(indentLevel+1, "(test) ");
-    body->Print(indentLevel+1, "(body) ");
+  test->Print(indentLevel+1, "(test) ");
+  body->Print(indentLevel+1, "(body) ");
 }
 
-IfStmt::IfStmt(Expr *t, Stmt *tb, Stmt *eb): ConditionalStmt(t, tb) { 
-    Assert(t != NULL && tb != NULL); // else can be NULL
-    elseBody = eb;
-    if (elseBody) elseBody->SetParent(this);
+IfStmt::IfStmt(Expr *t, Stmt *tb, Stmt *eb): ConditionalStmt(t, tb) {    Assert(t != NULL && tb != NULL); // else can be NULL
+  elseBody = eb;
+  if (elseBody) elseBody->SetParent(this);
 }
 
 void IfStmt::PrintChildren(int indentLevel) {
-    test->Print(indentLevel+1, "(test) ");
-    body->Print(indentLevel+1, "(then) ");
-    if (elseBody) elseBody->Print(indentLevel+1, "(else) ");
+  test->Print(indentLevel+1, "(test) ");
+  body->Print(indentLevel+1, "(then) ");
+  if (elseBody) elseBody->Print(indentLevel+1, "(else) ");
 }
 
+CaseStmt::CaseStmt(IntConstant* label, List<Stmt*>* body) : label(label), body(body) {
+  this->label->SetParent(this);
+  this->body->SetParentAll(this);
+}
 
-ReturnStmt::ReturnStmt(yyltype loc, Expr *e) : Stmt(loc) { 
-    Assert(e != NULL);
-    (expr=e)->SetParent(this);
+void CaseStmt::PrintChildren(int identLevel) {
+  this->label->Print(identLevel+1, "(label) ");
+  this->body->PrintAll(identLevel+1, "(body) ");
+}
+
+SwitchStmt::SwitchStmt(Expr* test, List<CaseStmt*>* cases, Stmt* defaultCase)
+  : test(test), cases(cases), defaultCase(defaultCase) {
+  this->test->SetParent(this);
+  this->cases->SetParentAll(this);
+  this->defaultCase->SetParent(this);
+}
+
+void SwitchStmt::PrintChildren(int identLevel) {
+  this->test->Print(identLevel+1, "(test) ");
+  this->cases->PrintAll(identLevel+1, NULL);
+  this->defaultCase->Print(identLevel+1, "(default) ");
+}
+
+ReturnStmt::ReturnStmt(yyltype loc, Expr *e) : Stmt(loc) {    Assert(e != NULL);
+  (expr=e)->SetParent(this);
 }
 
 void ReturnStmt::PrintChildren(int indentLevel) {
-    expr->Print(indentLevel+1);
+  expr->Print(indentLevel+1);
 }
-  
-PrintStmt::PrintStmt(List<Expr*> *a) {    
-    Assert(a != NULL);
-    (args=a)->SetParentAll(this);
+PrintStmt::PrintStmt(List<Expr*> *a) {    Assert(a != NULL);
+  (args=a)->SetParentAll(this);
 }
 
 void PrintStmt::PrintChildren(int indentLevel) {
-    args->PrintAll(indentLevel+1, "(args) ");
+  args->PrintAll(indentLevel+1, "(args) ");
 }
 
 
--- ast_stmt.h
+++ ast_stmt.h
@@ -3,8 +3,7 @@
  * The Stmt class and its subclasses are used to represent
  * statements in the parse tree.  For each statment in the
  * language (for, if, return, etc.) there is a corresponding
- * node class for that construct. 
- *
+ * node class for that construct. *
  * pp2: You will need to add new expression and statement node c
  * classes for the additional grammar elements (Switch/Postfix)
  */
@@ -19,112 +18,118 @@
 class Decl;
 class VarDecl;
 class Expr;
-  
+class IntConstant;
 class Program : public Node
 {
-  protected:
-     List<Decl*> *decls;
-     
-  public:
-     Program(List<Decl*> *declList);
-     const char *GetPrintNameForNode() { return "Program"; }
-     void PrintChildren(int indentLevel);
+protected:
+  List<Decl*> *decls;
+public:
+  Program(List<Decl*> *declList);
+  const char *GetPrintNameForNode() { return "Program"; }
+  void PrintChildren(int indentLevel);
 };
 
 class Stmt : public Node
 {
-  public:
-     Stmt() : Node() {}
-     Stmt(yyltype loc) : Node(loc) {}
+public:
+  Stmt() : Node() {}
+  Stmt(yyltype loc) : Node(loc) {}
 };
 
-class StmtBlock : public Stmt 
-{
-  protected:
-    List<VarDecl*> *decls;
-    List<Stmt*> *stmts;
-    
-  public:
-    StmtBlock(List<VarDecl*> *variableDeclarations, List<Stmt*> *statements);
-    const char *GetPrintNameForNode() { return "StmtBlock"; }
-    void PrintChildren(int indentLevel);
+class StmtBlock : public Stmt{
+protected:
+  List<VarDecl*> *decls;
+  List<Stmt*> *stmts;
+public:
+  StmtBlock(List<VarDecl*> *variableDeclarations, List<Stmt*> *statements);
+  const char *GetPrintNameForNode() { return "StmtBlock"; }
+  void PrintChildren(int indentLevel);
 };
 
-  
 class ConditionalStmt : public Stmt
 {
-  protected:
-    Expr *test;
-    Stmt *body;
-  
-  public:
-    ConditionalStmt(Expr *testExpr, Stmt *body);
+protected:
+  Expr *test;
+  Stmt *body;
+public:
+  ConditionalStmt(Expr *testExpr, Stmt *body);
 };
 
-class LoopStmt : public ConditionalStmt 
-{
-  public:
-    LoopStmt(Expr *testExpr, Stmt *body)
-            : ConditionalStmt(testExpr, body) {}
+class LoopStmt : public ConditionalStmt{
+public:
+  LoopStmt(Expr *testExpr, Stmt *body)
+    : ConditionalStmt(testExpr, body) {}
 };
 
-class ForStmt : public LoopStmt 
-{
-  protected:
-    Expr *init, *step;
-  
-  public:
-    ForStmt(Expr *init, Expr *test, Expr *step, Stmt *body);
-    const char *GetPrintNameForNode() { return "ForStmt"; }
-    void PrintChildren(int indentLevel);
+class ForStmt : public LoopStmt{
+protected:
+  Expr *init, *step;
+public:
+  ForStmt(Expr *init, Expr *test, Expr *step, Stmt *body);
+  const char *GetPrintNameForNode() { return "ForStmt"; }
+  void PrintChildren(int indentLevel);
 };
 
-class WhileStmt : public LoopStmt 
-{
-  public:
-    WhileStmt(Expr *test, Stmt *body) : LoopStmt(test, body) {}
-    const char *GetPrintNameForNode() { return "WhileStmt"; }
-    void PrintChildren(int indentLevel);
+class WhileStmt : public LoopStmt{
+public:
+  WhileStmt(Expr *test, Stmt *body) : LoopStmt(test, body) {}
+  const char *GetPrintNameForNode() { return "WhileStmt"; }
+  void PrintChildren(int indentLevel);
 };
 
-class IfStmt : public ConditionalStmt 
-{
-  protected:
-    Stmt *elseBody;
-  
-  public:
-    IfStmt(Expr *test, Stmt *thenBody, Stmt *elseBody);
-    const char *GetPrintNameForNode() { return "IfStmt"; }
-    void PrintChildren(int indentLevel);
+class IfStmt : public ConditionalStmt{
+protected:
+  Stmt *elseBody;
+public:
+  IfStmt(Expr *test, Stmt *thenBody, Stmt *elseBody);
+  const char *GetPrintNameForNode() { return "IfStmt"; }
+  void PrintChildren(int indentLevel);
 };
 
-class BreakStmt : public Stmt 
-{
-  public:
-    BreakStmt(yyltype loc) : Stmt(loc) {}
-    const char *GetPrintNameForNode() { return "BreakStmt"; }
+class BreakStmt : public Stmt{
+public:
+  BreakStmt(yyltype loc) : Stmt(loc) {}
+  const char *GetPrintNameForNode() { return "BreakStmt"; }
 };
 
-class ReturnStmt : public Stmt  
-{
-  protected:
-    Expr *expr;
-  
-  public:
-    ReturnStmt(yyltype loc, Expr *expr);
-    const char *GetPrintNameForNode() { return "ReturnStmt"; }
-    void PrintChildren(int indentLevel);
+class CaseStmt : public Stmt {
+protected:
+  IntConstant* label;
+  List<Stmt*>* body;
+public:
+  CaseStmt(IntConstant* label, List<Stmt*>* body);
+  const char *GetPrintNameForNode() { return "CaseStmt";}
+  void PrintChildren(int identLevel);
+};
+
+class SwitchStmt : public Stmt {
+protected:
+  Expr* test;
+  List<CaseStmt*>* cases;
+  Stmt* defaultCase;
+public:
+  SwitchStmt(Expr *test, List<CaseStmt*>* cases, Stmt* defaultCase);
+  const char *GetPrintNameForNode() { return "SwitchStmt"; }
+  void PrintChildren(int identLevel);
+};
+
+class ReturnStmt : public Stmt{
+protected:
+  Expr *expr;
+public:
+  ReturnStmt(yyltype loc, Expr *expr);
+  const char *GetPrintNameForNode() { return "ReturnStmt"; }
+  void PrintChildren(int indentLevel);
 };
 
 class PrintStmt : public Stmt
 {
-  protected:
-    List<Expr*> *args;
-    
-  public:
-    PrintStmt(List<Expr*> *arguments);
-    const char *GetPrintNameForNode() { return "PrintStmt"; }
-    void PrintChildren(int indentLevel);
+protected:
+  List<Expr*> *args;
+public:
+  PrintStmt(List<Expr*> *arguments);
+  const char *GetPrintNameForNode() { return "PrintStmt"; }
+  void PrintChildren(int indentLevel);
 };
 
 
