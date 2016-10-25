604db83888d2ffbef9487c7095edc2ce359e55d7
Martin Fracker <martin.frackerjr@gmail.com>
2016-10-25 18:00:17 -0500

    Implement if statement emission

--- decaf/ast/stmt/if_stmt.cc
+++ decaf/ast/stmt/if_stmt.cc
@@ -1,6 +1,7 @@
 #include "if_stmt.hh"
 
 #include <ast/symbol_table.hh>
+#include <codegen/codegen.hh>
 
 IfStmt::IfStmt(Expr *t, Stmt *tb, Stmt *eb): ConditionalStmt(t, tb) {
   Assert(t != NULL && tb != NULL); // else can be NULL
@@ -29,4 +30,39 @@ void IfStmt::PrintChildren(int indentLevel) {
 void IfStmt::analyze(Symbol_table* symbol_table, reasonT focus) {
   ConditionalStmt::analyze(symbol_table, focus);
   if (elseBody) elseBody->analyze(symbol_table, focus);
+}
+
+void IfStmt::emit(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table) {
+  generate_labels(codegen);
+  test->emit(codegen, frame_allocator, symbol_table);
+  generate_test_jump(codegen, frame_allocator, symbol_table);
+  generate_body(codegen, frame_allocator, symbol_table);
+  generate_else(codegen, frame_allocator, symbol_table);
+  codegen->GenLabel(after_stmt_label);
+}
+
+void IfStmt::generate_labels(CodeGenerator* codegen) {
+  after_stmt_label = codegen->NewLabel();
+  else_body_label = codegen->NewLabel();
+  false_label = after_stmt_label;
+  if (elseBody)
+    false_label = else_body_label;
+}
+
+void IfStmt::generate_test_jump(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table) {
+  test_location = test->get_frame_location();
+  codegen->GenIfZ(test_location, false_label);
+}
+
+void IfStmt::generate_body(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table) {
+  body->emit(codegen, frame_allocator, symbol_table);
+  codegen->GenGoto(after_stmt_label);
+}
+
+void IfStmt::generate_else(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table) {
+  if (elseBody) {
+    codegen->GenLabel(else_body_label);
+    elseBody->emit(codegen, frame_allocator, symbol_table);
+    codegen->GenGoto(after_stmt_label);
+  }
 }
--- decaf/ast/stmt/if_stmt.hh
+++ decaf/ast/stmt/if_stmt.hh
@@ -13,6 +13,17 @@ public:
   void build_table() override;
   void analyze(Symbol_table* symbol_table, reasonT focus) override;
   void set_parent(Symbol_table& other) override;
+  void emit(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table) override;
+private:
+  char* after_stmt_label;
+  char* else_body_label;
+  char* false_label;
+  Location* test_location;
+
+  void generate_labels(CodeGenerator* codegen);
+  void generate_test_jump(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table);
+  void generate_body(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table);
+  void generate_else(CodeGenerator* codegen, Frame_allocator* frame_allocator, Symbol_table* symbol_table);
 };
 
 #endif /* IF_STMT_H */
