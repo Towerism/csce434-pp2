0aeba3f13734472470a24d74296d82400a3c6157
Martin Fracker <martin.frackerjr@gmail.com>
2016-11-05 14:39:31 -0500

    Make integration tests for runtime errors pass

--- decaf/codegen/error_message_factory.cc
+++ decaf/codegen/error_message_factory.cc
@@ -2,6 +2,8 @@
 
 #include <cstring>
 
+#include <sstream>
+
 const std::string
     Error_message_factory::error_messages[(int)Runtime_error::_Count_] = {
         "Decaf runtime error: Array size is <= 0",
@@ -9,5 +11,9 @@ const std::string
 
 char *Error_message_factory::for_runtime_error(Runtime_error error) {
   auto message = error_messages[(int)error];
-  return strdup(message.c_str());
+
+  std::ostringstream in;
+
+  in << message << "\\n";
+  return strdup(in.str().c_str());
 }
